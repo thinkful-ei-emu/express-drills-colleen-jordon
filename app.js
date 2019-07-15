83f2e6efdd70fb7eb4f69e6f07a4ca10030ea81e
Jvymvn <Jordon.carter@yahoo.com>
2019-07-15 16:05:48 -0400

    Drill 2 completed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express')
 const morgan = require('morgan')
 //invoking express top level function to create the app object
@@ -6,14 +8,15 @@ const app = express()
 // mounting morgan (middleware forlogging)
 app.use(morgan('dev'))
 //res: HTTP res is request object to send text to the client
-app.get('/', (req, res) => {res.send('Hello Express. I am living inside the computer')})
+app.get('/', (req, res) => { res.send('Hello Express. I am living inside the computer') })
 //listening to a specific port to correctly route request to 
 // the server:
-app.listen(8000, () =>{
+app.listen(8000, () => {
   console.log('Express server listening on port 8000')
 })
 
-app.get('/burger', (req, res)=>{
+
+app.get('/burger', (req, res) => {
   res.send('We have amazing delicious cheeseburgers! ')
 })
 app.get('/echo', (req, res) => {
@@ -31,21 +34,21 @@ app.get('/queryViewer', (req, res) => {
   res.end(); //do not send any data back to the client
 });
 
-app.get('/greetings', (req, res)=>{
+app.get('/icecream', (req, res) => {
   const name = req.query.name
   const race = req.query.race
 
-  if(!name) {
+  if (!name) {
     return res.status(400).send('Please provide a name')
   }
-  if(! race){
+  if (!race) {
     return res.status(400).send('Please provide a race')
   }
   const greeting = `Greetings, ${name} the ${race}, welcome to Express`
   res.send(greeting);
 })
 
-app.get('/sum', (req, res)=>{
+app.get('/sum', (req, res) => {
   const textA = req.query.a
   const textB = req.query.b
   const numA = parseInt(textA, 10)
@@ -53,5 +56,23 @@ app.get('/sum', (req, res)=>{
   const sum = numA + numB
   const answer = `The sum of ${numA} and ${numB} is ${sum}`
   res.send(answer);
+})
 
+
+
+app.get('/cipher', (req, res) => {
+
+  const param1 = req.query.text.toUpperCase();
+  const param2 = parseInt(req.query.shift, 10);
+  let output = '';
+
+  for (let i = 0; i < param1.length; i++) {
+    let c = param1[i];
+    let code = param1.charCodeAt(i);
+    c = String.fromCharCode((((code - 65 + param2) % 26) + 65));
+    output += c;
+    console.log(output);
+  }
+
+  res.send(output);
 })
