94ad224abc89041c2146e729284f44eab39da2ba
Jvymvn <Jordon.carter@yahoo.com>
2019-07-15 21:32:01 -0400

    Drill 3 Updates

--- app.js
+++ app.js
@@ -79,25 +79,47 @@ app.get("/cipher", (req, res) => {
 
 /* To send an array of values to the server via a query string simply repeat the key with different values. For instance, the query string ?arr=1&arr=2&arr=3 results in the query object { arr: [ '1', '2', '3' ] }. Create a new endpoint /lotto that accepts an array of 6 distinct numbers between 1 and 20 named numbers. The function then randomly generates 6 numbers between 1 and 20. Compare the numbers sent in the query with the randomly generated numbers to determine how many match. If fewer than 4 numbers match respond with the string "Sorry, you lose". If 4 numbers match respond with the string "Congratulations, you win a free ticket", if 5 numbers match respond with "Congratulations! You win $100!". If all 6 numbers match respond with "Wow! Unbelievable! You could have won the mega millions!". */
 
-app.get('/lotto', (req, res)=> {
+app.get('/lotto', (req, res) => {
+
+  let numbers = req.query.arr;
 
   function getRandomArbitraryArray(min, max) {
-    let lottoNumbers = []
-    let number = 0
-    for (let i=6; i >0; i--){
-    number = Math.random() * (max - min) + min;
-    lottoNumbers.push(number)
-  }
-  console.log(lottoNumbers)
-  return lottoNumbers
-}
-getRandomArbitraryArray(1, 20)
-/* 
-  for (let i = 0; i < req.query.arr.length; i++){
+    let lottoNumbers = [];
+    let number = 0;
+    let newArray = [];
 
-  } */
-})
+    for (let i = 6; i > 0; i--) {
+      number = Math.floor(Math.random() * (max - min) + min);
+      lottoNumbers.push(number);
+    }
+
+    // console.log(lottoNumbers);
+    return lottoNumbers;
+  }
+
+  let lottos = getRandomArbitraryArray(1, 20);
+
+  function compareLotto(lottoNumbers, numbers) {
+    let winners = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      let newArray = [];
+      newArray.push(parseInt(numbers[i]));
+
+      newArray.forEach(int => {
+        for (let j = 0; j < lottoNumbers.length; j++) {
+          if (int === lottoNumbers[j]) {
+            winners += 1;
+          }
+        }
+      });
+    }
+    console.log(winners);
+    return winners;
+  }
+  let result = compareLotto(lottos, numbers);
+  res.send(result);
+});
 
 app.listen(8502, () => {
-  console.log("Express server listening on port 8000");
+  console.log("Express server listening on port 8502");
 });
